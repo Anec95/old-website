7078375d6f1bcff1c4b91ca30d9d4d3da1b8abcc
Anec95 <113508749+Anec95@users.noreply.github.com>
2022-09-23 16:27:56 +0200

    Add files via upload
    pulizia dei css

---
+++ portfolio/index.js
@@ -0,0 +1,65 @@
+const imgArray = ["../images/portfolio/1.png", "../images/portfolio/2.png", "../images/portfolio/3.png", "../images/portfolio/4.png", "../images/portfolio/5.png", "../images/portfolio/4.png", "../images/portfolio/6.png"];
+    
+    let index = 0;
+                
+    const imghtml = document.getElementById("photo-portfolio");
+    const rightArrow = document.getElementById("right-arrow");
+    const leftArrow = document.getElementById("left-arrow");
+    const pageNumber = document.getElementById("photo-number");
+    
+    imghtml.src = imgArray[index];
+    pageNumber.innerHTML = (index + 1) + " di " + imgArray.length;
+
+if(index!==0) {
+   leftArrow.style.display="block";
+}
+
+function nextImage() {
+    if(index < imgArray.length -1) {
+        console.log("Clicked on next page");
+        console.log("current index is " + index);
+
+        index++;
+    } else {
+        console.log("There are no more pages available... restarting");
+        console.log("current index is " + index);
+
+        index=0;
+    }                    
+
+    if(index===0) {
+        leftArrow.style.display="none";                        
+    } else {
+        leftArrow.style.display="block";
+    }
+
+    pageNumber.innerHTML = (index + 1) + " di " + imgArray.length;
+    imghtml.src = imgArray[index];
+};
+
+function prevImage() {
+    if(index > 0) {
+        console.log("Clicked on next page");
+        console.log("current index is " + index);
+
+        index--;
+    } else {
+        console.log("There are no prev pages available");
+        console.log("current index is " + index);
+
+        index=0;
+    }
+
+    if(index===0) {
+        leftArrow.style.display="none";
+    } else {
+        leftArrow.style.display="block";
+    }
+
+    pageNumber.innerHTML = (index + 1) + " di " + imgArray.length;
+    imghtml.src = imgArray[index];
+};
+
+leftArrow.addEventListener("click", prevImage);
+
+rightArrow.addEventListener("click", nextImage);

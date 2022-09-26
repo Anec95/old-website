0a4e3725f731e501903b03d38fa3c277efa5a2d2
Anec95 <113508749+Anec95@users.noreply.github.com>
2022-09-26 15:00:15 +0200

    Add files via upload
    aggiunta immagini portfolio

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,4 +1,4 @@
-const imgArray = ["../images/portfolio/1.png", "../images/portfolio/2.png", "../images/portfolio/3.png", "../images/portfolio/4.png", "../images/portfolio/5.png", "../images/portfolio/4.png", "../images/portfolio/6.png"];
+const imgArray = ["../images/portfolio/page-1.png", "../images/portfolio/page-2.png", "../images/portfolio/page-3.png", "../images/portfolio/page-4.png", "../images/portfolio/page-5.png", "../images/portfolio/page-6.png", "../images/portfolio/page-7.png", "../images/portfolio/page-8.png"];
     
     let index = 0;
                 
